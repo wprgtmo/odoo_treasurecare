2a97d7dabdb2ba565fd37a486f55d184215cb440
wilfredo <wprgtmo@gmail.com>
2024-04-17 11:41:14 -0400

    Importación de las llamadas de zonitell

--- mis_addons/zonitell/models/zonitell_call_import.py
+++ mis_addons/zonitell/models/zonitell_call_import.py
@@ -17,76 +17,126 @@ class ZonitellCallImport(models.TransientModel):
 
     file = fields.Binary(string="File", required=True)
 
+    def create_contact(self, name, phone, is_company=False):
+        name =  str(name).title() if name else "Sin nombre"
+        phone = phone if phone else 0
+        contact = self.env['res.partner'].search([('name', '=', name), ('phone', '=' , phone)])
+        if not contact:
+            contact= self.env['res.partner'].create({
+                                            'name': name,
+                                            'phone': phone,
+                                            'is_company': is_company,
+                                            })
+        return contact
+
+    def create_extension_contact(self, name, ext, parent):
+        name = str(name).title() if name else "Sin extension"
+        ext = ext if ext else 0
+        parent = parent if parent else 0
+        extension = self.env['res.partner'].search([('name', '=', name), ('phone', '=', ext), ('parent_id', '=', parent)])
+        if not extension:   
+            extension= self.env['res.partner'].create({
+                                            'name': name,
+                                            'phone': ext,
+                                            'is_company': False,
+                                            'parent_id':  parent,
+                                            'commercial_partner_id': parent,
+                                            })
+        return extension
+
+
+    def update_extension_contact(self, name, ext, parent):
+        extension = self.env['res.partner'].search([('phone', '=', ext), ('parent_id', '=', parent)])
+        extension.name = name
+
+    def create_duracere_contacts(self): 
+        duracare= self.create_contact('Duracare Home Health Services', '7137820551', True)
+        # self.create_extension_contact('SARAY','101', duracare.id)
+        # self.create_extension_contact('THAIS','102', duracare.id)
+        # self.create_extension_contact('JEANETTE','103', duracare.id)
+        # self.create_extension_contact(duracare.name,duracare.id, duracare.id)
+        return duracare
+
+    def create_call(self, record)->any:
+        return self.env['zonitell.call'].create({
+            'direction': record[1],
+            'status': record[2],
+            'extension': record[3],
+            'caller_id_name': record[4],
+            'caller_id_number': record[5],
+            'caller_destination': record[6],
+            'caller_destination_number':record[7],
+            'call_date': datetime.strptime(record[8], "%m/%d/%Y").date(),
+            'start_stamp':record[9],
+            'end_stamp':record[10],
+            'duration_seconds':record[11],
+            'missed_call': record[12],
+            'hangup_cause': record[13],
+            'notes': record[14],
+        })
+
+    def create_relation(self, call, contact, direction):
+        return self.env['zonitell.call.contact'].create({
+            'call_id': call,
+            'contact_id': contact,
+            'direction' : direction
+        })
+
+    def clean(self):
+        self.env['zonitell.call'].search([]).unlink()
+        self.env['res.partner'].search([('phone', '!=', '+1 813-505-3406')]).unlink()
+        self.env['zonitell.call.contact'].search([]).unlink()
+
 
     def calls_import(self):
         try:
+            self.clean()
+            duracare = self.create_duracere_contacts()
             wb = openpyxl.load_workbook(BytesIO(base64.b64decode(self.file)), read_only=True)
             ws = wb.active
           
             for record in ws.iter_rows(min_row=2,values_only=True):
 
-                # Procesando Calls
-                call = self.env['zonitell.call'].create({
-                    'company_name': record[0],
-                    'direction': record[1],
-                    'status': record[2],
-                    'extension': record[3],
-                    'caller_id_name': record[4],
-                    'caller_id_number': record[5],
-                    'caller_destination': record[6],
-                    'caller_destination_number':record[7],
-                    'call_date': datetime.strptime(record[8], "%m/%d/%Y").date(),
-                    'start_stamp':record[9],
-                    'end_stamp':record[10],
-                    'duration_seconds':record[11],
-                    'missed_call': record[12],
-                    'hangup_cause': record[13],
-                    'notes': record[14],
-                })
+                if record[1]=='outbound': # The calls is outbound
+                    # Procesando Calls
+                    call= self.create_call(record)
 
-                #Procesando Contactos
+                    #Procesando Contactos
+                    extension = self.create_extension_contact(record[3], record[3], duracare.id)
+                    contact= self.create_contact(record[6] , record[7]) 
 
-                contact_name = str(record[4]).title()
-                contact = self.env['res.partner'].search([('name', '=', contact_name)])
-                if not contact:
-                    contact=self.env['res.partner'].create({
-                        # 'patient_id':patiend.id,
-                        # 'contact_relation_id':relation.id,
-                        'name': contact_name,
-                        'phone': record[5]
-                        })
+                    #Procesando Relacion 
+                    self.create_relation(call.id, extension.id, 'out')
+                    self.create_relation(call.id, contact.id, 'in')
 
-                #_logger.debug(f'{call.id:10} ==> {contact.id:10}')
+                if record[1]=='inbound': # The calls is inbound
+                    # Procesando Calls
+                    call= self.create_call(record)
 
-                #Procesando Relacion 
+                    #Procesando Contactos
+                    extension = self.create_extension_contact(record[3], record[3], duracare.id)
+                    contact= self.create_contact(record[4] , record[5]) 
 
-                # if call and contact:
-                self.env['zonitell.call.contact'].create({
-                        'call_id': call.id,
-                        'contact_id': contact.id,
-                        'direction' : record[1]
-                    })
-
-                # intake = self.env['hc.intake'].create({
-                #     'start_date': datetime.strptime(record[25], "%m/%d/%Y").date(), 
-                #     'end_date': datetime.strptime(record[26], "%m/%d/%Y").date(), 
-                #     'start_care_date': datetime.strptime(record[24], "%m/%d/%Y").date(), 
-                #     'note_diagnostic_primary': str(record[28]).title(),
-                #     'note_diagnostic_secondary': str(record[29]).title(),
-                #     'disciplines': str(record[30]).title(),
-                #     'disciplines_frecuencies': str(record[31]).title(),
-                #     'patient_id': patient.id,
-                #     'physician_id': physician.id
-                # })    
-            
+                    #Procesando Relacion 
+                    self.create_relation(call.id, extension.id, 'in')
+                    self.create_relation(call.id, contact.id, 'out')    
 
 
+                if record[1]=='local': # The calls is inbound
+                    # Procesando Calls
+                    call= self.create_call(record)
 
+                    #Procesando Contactos
+                    dest = self.create_extension_contact(record[3], record[3], duracare.id)
+                    origen= self.create_extension_contact(record[4] , record[5], duracare.id) 
 
-                        
-                            
+                    #Procesando Relacion 
+                    self.create_relation(call.id, dest.id, 'in')
+                    self.create_relation(call.id, origen.id, 'out')                                         
 
-
+            self.update_extension_contact('SARAY','101', duracare.id)
+            self.update_extension_contact('THAIS','102', duracare.id)
+            self.update_extension_contact('JEANETTE','103', duracare.id)
         except: 
             raise
             
