295384783e9e2720c83ae47e6f923b259d32114b
wprgtmo <wprgtmo@gmail.com>
2024-07-30 11:58:24 -0500

    Se quitó el modulo de zonitell

--- mis_addons/zonitell/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import zonitell_call
-from . import zonitell_contact
-from . import zonitell_call_contact
-from . import zonitell_call_import
--- mis_addons/zonitell/models/zonitell_contact.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-
-class ZonitellContact(models.Model):    
-    _name = 'res.partner'
-    _inherit = 'res.partner'
-
-    call_ids = fields.One2many(comodel_name='zonitell.call.contact', inverse_name="contact_id")
